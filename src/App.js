65182ed8eda0857e8b83745228b277b155b13970
peggy1008123 <peggy1008123@yahoo.com.tw>
2021-05-31 18:57:01 +0800

    ya

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Product";
+import DetailPage from "./pages/Details";
+import ShoppingPage from "./pages/Shopping";
+import Compare from "./pages/Compare";
+
+import { StoreProvider } from "./context/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/product" component={ProductPage} />
+          <Route exact path="/product/:product" component={ProductPage} />
+          <Route exact path="/detail/:productId" component={DetailPage} />
+          <Route exact path="/shoppingbag" component={ShoppingPage} />
+          <Route exact path="/compare" component={Compare} />
+        </Switch>
+      </BrowserRouter>
+    </StoreProvider>
   );
 }
 
